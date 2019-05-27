ec0d7815c1c524363113a5577c7be2c6312bcada
MoNTE48 <MoNTE48@mail.ua>
2019-05-27 18:49:25 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- How often to report used memory
+local log_time = 3
+
+-- Value before collecting, MB
+local max_ram = 500
+
+local function collectgarbage_info()
+	minetest.after(log_time, function()
+		minetest.log("warning", "[GARBAGE] Used ".. collectgarbage("count")/1024 .." MB.")
+		collectgarbage_info()
+	end)	
+end
+
+minetest.register_on_joinplayer(function(player)
+	collectgarbage_info()
+end)
+
+minetest.register_globalstep(function(dtime)
+	if collectgarbage("count") > (max_ram * 1024) then
+		collectgarbage()
+		minetest.log("warning", "[GARBAGE] Used more than ".. max_ram .." MB. Collecting Garbage...")
+	end
+end)
