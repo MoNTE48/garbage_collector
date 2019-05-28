518833782e0c971a92d3e5afb2d2b05b174b44bd
MoNTE48 <MoNTE48@mail.ua>
2019-05-28 19:53:13 +0200

    Improvement

--- init.lua
+++ init.lua
@@ -2,20 +2,17 @@
 local log_time = 5
 
 -- Value before collecting, MB
-local max_ram = 500
+local max_ram = 800
 
 local function collectgarbage_info()
 	minetest.after(log_time, function()
-		minetest.log("warning", "[GARBAGE] Used ".. math.ceil(collectgarbage("count")/1024) .." MB.")
+		minetest.log("warning", "[GARBAGE] Used ".. math.ceil(collectgarbage("count") / 1024) .." MB.")
+		if collectgarbage("count") > (max_ram * 1024) then
+			collectgarbage()
+			minetest.log("warning", "[GARBAGE] Used more than ".. max_ram .." MB. Collecting Garbage...")
+		end
 		collectgarbage_info()
 	end)	
 end
 
 collectgarbage_info()
-
-minetest.register_globalstep(function(dtime)
-	if collectgarbage("count") > (max_ram * 1024) then
-		collectgarbage()
-		minetest.log("warning", "[GARBAGE] Used more than ".. max_ram .." MB. Collecting Garbage...")
-	end
-end)
