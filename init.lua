4fee7d974b1918a74d92eb6756033d59c53a9e14
MoNTE48 <MoNTE48@mail.ua>
2020-03-26 20:24:50 +0100

    Simplify the code structure

--- init.lua
+++ init.lua
@@ -5,14 +5,13 @@ local log_time = 30
 local max_ram = 800
 
 local function collectgarbage_info()
-	minetest.after(log_time, function()
-		minetest.log("warning", "[GARBAGE] Used ".. math.ceil(collectgarbage("count") / 1024) .." MB.")
-		if collectgarbage("count") > (max_ram * 1024) then
-			collectgarbage()
-			minetest.log("warning", "[GARBAGE] Used more than ".. max_ram .." MB. Collecting Garbage...")
-		end
-		collectgarbage_info()
-	end)	
+	minetest.log("warning", "[GARBAGE] Used " .. math.ceil(collectgarbage("count") / 1024) .. " MB.")
+	if collectgarbage("count") > (max_ram * 1024) then
+		collectgarbage()
+		minetest.log("warning", "[GARBAGE] Used more than " .. max_ram .. " MB. Collecting Garbage...")
+	end
+
+	minetest.after(log_time, collectgarbage_info)
 end
 
-collectgarbage_info()
+minetest.after(log_time, collectgarbage_info)
